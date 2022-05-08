1697cf9849282090cb4e5c9a23acf30d3ef76268
campregher99 <f.campregher@studenti.unibs.it>
2022-05-08 21:29:09 +0200

    fine cinematica inversa

--- Matlab/PositionMat.m
+++ Matlab/PositionMat.m
@@ -1,9 +1,13 @@
-function [M01,M12,M23] = PositionMat(Q,L)
+function [M01_,M1_1,M12,M23] = PositionMat(Q,L)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-M01=[1 0 0 L(1)*cos(Q(1))+L(2);
+M01_=[1 0 0 L(1)*cos(Q(1));
     0 1 0 0;
     0 0 1 -L(1)*sin(Q(1));
+    0 0 0 1];
+M1_1=[1 0 0 L(2);
+    0 1 0 0;
+    0 0 1 0;
     0 0 0 1];
 M11_=[cos(pi/4) 0 sin(pi/4) 0;
     0 1 0 0;
@@ -13,12 +17,17 @@ M1_2_=[cos(Q(2)) -sin(Q(2)) 0 0;
     sin(Q(2)) cos(Q(2)) 0 0;
     0 0 1 0;
     0 0 0 1];
-M12_=M11_*M1_2_*inv(M11_);
-M2_2=[cos(pi/4) 0 sin(pi/4) L(3)*cos(pi/4);
+M12_=M11_*M1_2_;
+M2_2_=[sin(pi/4) 0 -sin(pi/4) 0;
     0 1 0 0;
-    -sin(pi/4) 0 cos(pi/4) L(3)*cos(pi/4);
+    sin(pi/4) 0 cos(pi/4) 0;
     0 0 0 1];
-M12=M12_*M2_2*inv(M12_);
+M12__=M12_*M2_2_;
+M2_2=[1 0 0 L(3);
+    0 1 0 0;
+    0 0 1 0;
+    0 0 0 1];
+M12=M12__*M2_2;
 M23_=[cos(Q(3)) 0 sin(Q(3)) 0;
         0 1 0 0;
         -sin(Q(3)) 0 cos(Q(3)) 0;
@@ -27,6 +36,6 @@ M3_3=[1 0 0 L(4);
         0 1 0 0;
         0 0 1 0;
         0 0 0 1];
-M23=M23_*M3_3*inv(M23_);
+M23=M23_*M3_3;
 end
 
